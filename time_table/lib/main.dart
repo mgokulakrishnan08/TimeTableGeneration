0a01ee4e717f332e553b62c495251fbd15be2fb7
Bala Murali Krishna <balamk2404@gmail.com>
2023-03-02 22:44:33 +0530

    home UI created

--- time_table/lib/main.dart
+++ time_table/lib/main.dart
@@ -1,115 +1,170 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(const MyApp());
 }
+class MyApp extends StatelessWidget{
+const MyApp({Key?key}) :super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+@override
+Widget build(BuildContext context){
+  var materialApp = MaterialApp(
+    title: 'TimetableGeneration',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: const AddUser(),
+  );
+  return materialApp;
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class AddUser extends StatefulWidget {
+  const AddUser({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  State<AddUser> createState() => _AddUserState();
+}
+class _AddUserState extends State<AddUser>{
+  final _userNameController = TextEditingController();
+  final _userFacultyController = TextEditingController();
+  final _userDaysController = TextEditingController();
+  final _userPeriodsController = TextEditingController();
+   bool _validateName = false;
+    bool _validateFaculty = false;
+     bool _validateDays = false;
+      bool _validatePeriods = false;
+     @override
+     Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("TimeTableGenerator"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        body: SingleChildScrollView(
+          child:Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Details",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal,
+                    fontWeight: FontWeight.w500
+
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  controller: _userNameController,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'Enter Class Name',
+                    labelText: 'ClassName',
+                    errorText: _validateName?'Class name cant be empty':null,
+                  ),
+                ),
+                const SizedBox(
+                  height:20.0,
+                ),
+                TextField(
+                  controller: _userFacultyController,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'Enter Faculty Advisor Name',
+                    labelText: 'FacultyAdvisorName',
+                    errorText: _validateFaculty?"Faculty name can't be empty":null,
+                  ),
+                ),
+                const SizedBox(
+                  height:20.0,
+                ),
+                TextField(
+                  controller: _userDaysController,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'Enter Number of Days',
+                    labelText: 'Number of Days',
+                    errorText: _validateDays?'Number of days field is required':null,
+                  ),
+                ),
+                const SizedBox(
+                  height:20.0,
+                ),
+                TextField(
+                  controller: _userPeriodsController,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'Enter Number of Periods',
+                    labelText: 'Number of Periods',
+                    errorText: _validatePeriods?'Number of Periods field is required':null,
+                  ),
+                ),
+                const SizedBox(
+                  height:20.0,
+                ),
+                Row(
+                children: [
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          primary: Colors.white,
+                          backgroundColor: Colors.teal,
+                          textStyle: const TextStyle(fontSize: 15)),
+                      onPressed: () async {
+                        setState(() {
+                          _userNameController.text.isEmpty
+                              ? _validateName = true
+                              : _validateName = false;
+                          _userFacultyController.text.isEmpty
+                              ? _validateFaculty = true
+                              : _validateFaculty = false;
+                          _userDaysController.text.isEmpty
+                              ? _validateDays = true
+                              : _validateDays = false;
+                          _userPeriodsController.text.isEmpty
+                              ? _validatePeriods = true
+                              : _validatePeriods = false;
+
+                        });
+                        /*if (_validateName == false &&
+                            _validateContact == false &&
+                            _validateDescription == false) {
+                         // print("Good Data Can Save");
+                          var _user = User();
+                          _user.name = _userNameController.text;
+                          _user.contact = _userContactController.text;
+                          _user.description = _userDescriptionController.text;
+                          var result=await _userService.SaveUser(_user);
+                         Navigator.pop(context,result);
+                        }*/
+                      },
+                      child: const Text('Next')),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          primary: Colors.white,
+                          backgroundColor: Colors.red,
+                          textStyle: const TextStyle(fontSize: 15)),
+                      onPressed: () {
+                        _userNameController.text = '';
+                        _userFacultyController.text = '';
+                        _userDaysController.text = '';
+                         _userPeriodsController.text = '';
+                      },
+                      child: const Text('Clear Details'))
+                ],
+              )
+              ],
+            ),
+            )
+           ),
+      );
+     }
+
+
+
+
+
 }
